648660d8cea5aaa67cc5465a6e1733b77a7cda1b
NothingSoup <linglijs@gmail.com>
2017-08-15 23:16:46 -0700

    removed unecessary files.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AtlasImage from './Art/2DArt/Atlas/Atlas.png';
-import MapsJSON from './atlasmaps.json';
+// import AtlasImage from './Art/2DArt/Atlas/Atlas.png';
+// import MapsJSON from './atlasmaps.json';
 
-var atlasMap = {
-  backgroundImage: `url(${AtlasImage})`,
-  width: "2048px",
-  height: "1152px",
-} 
+// var atlasMap = {
+//   backgroundImage: `url(${AtlasImage})`,
+//   width: "2048px",
+//   height: "1152px",
+// } 
 class App extends Component {
-  drawMap(map) {
-    var path = './' + map.icon_path;
-    var xMult = (2048/2) / 499.667;
+  // drawMap(map) {
+  //   var path = './' + map.icon_path;
+  //   var xMult = (2048/2) / 499.667;
 
-    var mapStyle = {
-      backgroundImage: 'url(' + require(`${path}`) + ')',
-      backgroundRepeat: 'no-repeat',
-      position: 'absolute',
-      width: 19 + 'px',
-      height: 19 + 'px',
-      padding: 9 + 'px',
-      left: `${map.x * xMult - 20/2}px`,
-      top: `${map.y * xMult - 20/2}px`,
-      backgroundSize: `19px 19px`,
-    };
-    console.log(path)
-    return <div style={mapStyle}></div>
-    // return <img src={require(`${path}`)} style={mapStyle} />
-  }
+  //   var mapStyle = {
+  //     backgroundImage: 'url(' + require(`${path}`) + ')',
+  //     backgroundRepeat: 'no-repeat',
+  //     position: 'absolute',
+  //     width: 19 + 'px',
+  //     height: 19 + 'px',
+  //     padding: 9 + 'px',
+  //     left: `${map.x * xMult - 20/2}px`,
+  //     top: `${map.y * xMult - 20/2}px`,
+  //     backgroundSize: `19px 19px`,
+  //   };
+  //   console.log(path)
+  //   return <div style={mapStyle}></div>
+  //   // return <img src={require(`${path}`)} style={mapStyle} />
+  // }
   
-  drawMaps(maps) {
-    return maps.map(this.drawMap);
-  }
+  // drawMaps(maps) {
+  //   return maps.map(this.drawMap);
+  // }
   render() {
     return (
-      <div style={ atlasMap }>
-        { this.drawMaps(MapsJSON) }
-      </div>
+      <div>Hello Atlas</div>
+      // <div style={ atlasMap }>
+        // {{ this.drawMaps(MapsJSON) }}
+      // </div>
     );
   }
 }
