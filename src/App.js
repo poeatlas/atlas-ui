afee0ca539925ecd6c72340cf51076691d72b32b
NothingSoup <linglijs@gmail.com>
2017-08-15 21:56:54 -0700

    initial test setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AtlasImage from './Art/2DArt/Atlas/Atlas.png';
+import MapsJSON from './atlasmaps.json';
 
+var atlasMap = {
+  backgroundImage: `url(${AtlasImage})`,
+  width: "2048px",
+  height: "1152px",
+} 
 class App extends Component {
+  drawMap(map) {
+    var path = './' + map.icon_path;
+    var xMult = (2048/2) / 499.667;
+
+    var mapStyle = {
+      backgroundImage: 'url(' + require(`${path}`) + ')',
+      backgroundRepeat: 'no-repeat',
+      position: 'absolute',
+      width: 19 + 'px',
+      height: 19 + 'px',
+      padding: 9 + 'px',
+      left: `${map.x * xMult - 20/2}px`,
+      top: `${map.y * xMult - 20/2}px`,
+      backgroundSize: `19px 19px`,
+    };
+    console.log(path)
+    return <div style={mapStyle}></div>
+    // return <img src={require(`${path}`)} style={mapStyle} />
+  }
+  
+  drawMaps(maps) {
+    return maps.map(this.drawMap);
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={ atlasMap }>
+        { this.drawMaps(MapsJSON) }
       </div>
     );
   }
