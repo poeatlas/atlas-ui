e82111429381e0cdabbc221bbfff5cc20861b4d3
NothingSoup <linglijs@gmail.com>
2017-09-08 00:13:45 -0700

    added popovers to display map names on hover.

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import atlas from './resources/atlas.json';
 import Map from './components/Map';
 
 class App extends Component {
-  
+
   render() {
     return (
       <div id="atlas">
-         { atlas.map((map) => <Map x={map.x} y={map.y} path={map.icon_path} key={map.id} /> ) }
+         { atlas.map((map) => <Map x={map.x} y={map.y} path={map.icon_path} name={map.world_areas_name} key={map.id} /> ) }
       </div>
     );
   }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import { MAP_MULTIPLIER, MAP_OFFSET } from '../const';
+import { Popover, OverlayTrigger } from 'react-bootstrap';
 
 class Map extends Component {
+
   render() {
-    const {path, x, y} = this.props;
+    const {path, x, y, name} = this.props;
+
+    const popoverHoverFocus = (
+      <Popover id="popover-trigger-hover-focus" title={name + " Map"}>
+        <strong>Tier: </strong> { name }
+      </Popover>
+    );
 
     const mapStyle = {
       backgroundImage: `url(./${path})`,
@@ -11,7 +19,11 @@ class Map extends Component {
       top: `${y * MAP_MULTIPLIER - MAP_OFFSET}px`,
     };
 
-    return <div className="map" style={mapStyle}></div>
+    return (
+      <OverlayTrigger trigger={['hover', 'focus']} placement="top" overlay={popoverHoverFocus}>
+        <div className="map" style={mapStyle}></div>
+      </OverlayTrigger>
+    );
   }
 }
 
