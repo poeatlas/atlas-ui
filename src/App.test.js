c7293318d7f49f43038b6993dc978758ebb4b454
NothingSoup <linglijs@gmail.com>
2017-09-21 23:55:57 -0700

    added provider to app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import MapStore from './stores/MapStore';
+import { Provider  } from 'mobx-react';
 
 it('renders without crashing', () => {
+  const stores = {
+    MapStore,
+  }
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider {...stores}>
+      <App />
+    </Provider>, div);
 });
