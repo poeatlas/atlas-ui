a9c1adde246faa54dec32a14eac088663f0baa24
NothingSoup <linglijs@gmail.com>
2017-10-04 19:47:23 -0700

    added missing store/array to app.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import MapStore from './stores/MapStore';
+import AtlasStore from './stores/AtlasStore';
 import { Provider  } from 'mobx-react';
+import atlas from './resources/atlas.json';
+import MapStore from './stores/MapStore';
 
 it('renders without crashing', () => {
+  const maps = [];
+
+  for (var i = 0; i < atlas.length; i++) {
+    const currMap = atlas[i];
+    const mapStore = new MapStore( currMap );
+
+    maps.push(mapStore);
+  }
+  
   const stores = {
-    MapStore,
+    AtlasStore,
   }
   const div = document.createElement('div');
   ReactDOM.render(
     <Provider {...stores}>
-      <App />
+      <App mapStore={maps}/>
     </Provider>, div);
 });
