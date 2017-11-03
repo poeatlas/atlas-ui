9c7b83cd9f7bec02f3d4508d5b2f5c18933b4e20
Ling Li <linglijs@gmail.com>
2017-11-02 23:23:58 -0700

    added router to app spec

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider  } from 'mobx-react';
+import { BrowserRouter } from 'react-router-dom'
+
 import atlas from './resources/atlas.json';
 import MapStore from './stores/MapStore';
 import AtlasStore from './stores/AtlasStore';
@@ -20,6 +22,8 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <Provider {...stores}>
-      <App mapStore={maps}/>
+      <BrowserRouter>
+        <App mapStore={maps}/>
+      </BrowserRouter>
     </Provider>, div);
 });
